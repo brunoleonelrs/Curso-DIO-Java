de04fc47d9ce573e9f2b0a2ba9c255494f4fd21c
Bruno Pereira <brunoleonelbl@outlook.com.br>
2026-01-02 10:12:54 -0300

    Corrigido espaçãmento e adicionado a opção de mudar canal ou selecionar o mesmo

--- ProjetoTiposVariaveis/src/SmartTv.java
+++ ProjetoTiposVariaveis/src/SmartTv.java
@@ -4,16 +4,28 @@ public class SmartTv {
     int canal = 1;
     int volume = 15;
     
+    //função de ligar e desligar a tv
     public void ligar() {
         ligada = true;
     }
     public void desligar() {
         ligada = false;
     }
+    //função de aumentar e diminuir o volume da tv
     public void aumentarVolume() {
         volume++;
     }
     public void diminuirVolume() {
         volume--;
+    }
+    //função de mudar ou seleionar canal da tv
+    public void aumentarCanal() {
+        canal++;
+    }
+    public void diminuirCanal() {
+        canal--;
+    }
+    public void selecionarCanal(int novoCanal) {
+        canal = novoCanal;
     }
 }
--- ProjetoTiposVariaveis/src/Usuario.java
+++ ProjetoTiposVariaveis/src/Usuario.java
@@ -1,26 +1,39 @@
 public class Usuario {
     public static void main(String[] args) {
         SmartTv smartTv = new SmartTv();
-        
+        //informações iniciais da tv
         System .out.println("Tv ligada? " + smartTv.ligada);
         System .out.println("Canal atual: " + smartTv.canal);
         System .out.println("Volume atual: " + smartTv.volume);
+        
         //ligando a tv
         smartTv.ligar();
         System .out.println("Tv ligada? " + smartTv.ligada);
+        
         //desligando a tv
         smartTv.desligar();
         System .out.println("Tv ligada? " + smartTv.ligada);
+        
         //Chamando o comando para aumentar o volume da tv
         smartTv.aumentarVolume();
         smartTv.aumentarVolume();
         //exibindo o resultado
         System .out.println("Volume atual: " + smartTv.volume);
+        
         //Chamando o comando para diminuir o volume da tv
         smartTv.diminuirVolume();
         smartTv.diminuirVolume();
         smartTv.diminuirVolume();
         //exibindo o resultado
         System .out.println("Volume atual: " + smartTv.volume);
+        
+        //alterando o canal da tv e exibindo o resultado
+        smartTv.aumentarCanal();
+        System .out.println("Canal atual: " + smartTv.canal);
+        smartTv.diminuirCanal();
+        System .out.println("Canal atual: " + smartTv.canal);
+        //selecionando um canal específico, chama o método selecionarCanal e seleciona o canal desejado. No exemplo, canal 10.
+        smartTv.selecionarCanal(10);
+        System .out.println("Canal atual: " + smartTv.canal);
     }
 }
