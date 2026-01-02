053da38c6a479101b698b0e4e1ce288c84c82214
Bruno Pereira <brunoleonelbl@outlook.com.br>
2026-01-02 10:01:25 -0300

    Aplicando os comandos para ajuste de volume da TV

--- ProjetoTiposVariaveis/src/Usuario.java
+++ ProjetoTiposVariaveis/src/Usuario.java
@@ -11,5 +11,16 @@ public class Usuario {
         //desligando a tv
         smartTv.desligar();
         System .out.println("Tv ligada? " + smartTv.ligada);
-
+        //Chamando o comando para aumentar o volume da tv
+        smartTv.aumentarVolume();
+        smartTv.aumentarVolume();
+        //exibindo o resultado
+        System .out.println("Volume atual: " + smartTv.volume);
+        //Chamando o comando para diminuir o volume da tv
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        //exibindo o resultado
+        System .out.println("Volume atual: " + smartTv.volume);
+    }
 }
