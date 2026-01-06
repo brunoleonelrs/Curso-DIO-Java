92c1cd184e1ce693b12ce318f3e478e59d70b30f
brunoleonelrs <brunoleonelbl@outlook.com.br>
2026-01-06 08:48:58 -0300

    Adicionado MinhaClasse, para testes em execução dentro do terminal.

---
+++ Java-Terminal/MinhaClasse/README.md
@@ -0,0 +1,3 @@
+<h3>Como usar o programa através de executavel e/ou terminar.</h3>
+<br>
+<p>Aqui vamos explorar as saídas de dados de uma aplicação JAVA. Vamos criar um executal do programa e aprender também a lidar com ele através do terminal.</p>
---
+++ Java-Terminal/MinhaClasse/src/MinhaClasse.java
@@ -0,0 +1,15 @@
+public class MinhaClasse {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Oi, fui executado pelo terminal!");
+
+        /*
+        1. Abra o MS-DOS ou Power Shell do windows.
+        2. Localize o diretório onde o arquivo MinhaClasse.class está salvo.
+        3. Execute o comando: java MinhaClasse
+        4. Veja o resultado no terminal.
+        5. Lembre-se de que o arquivo .class deve estar no mesmo diretório onde você está executando o comando.
+        */
+
+
+    }
+}
